d3177110767beb9df87e4192e28d2151995bfba8
c0nstanta <konstantin.koshevoj@bk.ru>
2024-02-27 23:15:42 +0600

    debug of project

--- Assets/Scripts/Classes/Say.cs
+++ Assets/Scripts/Classes/Say.cs
@@ -35,8 +35,6 @@ public class Say : MonoBehaviour
     private void NextDialog()
     {
         if (dialogs == null) return;
-        DisActiveButtons();
-        text.gameObject.SetActive(true);
         _dialog ??= dialogs.nodes.Find(node => node is StoryStartNode);
         if (!_isTyping)
         {
@@ -52,7 +50,9 @@ public class Say : MonoBehaviour
                     break;
                 }
                 case Dialog dialog: {
+                    text.gameObject.SetActive(true);
                     ShowDialog(dialog);
+                    DisActiveButtons();
                     break;
                 }
                 case AuthorText authorText: {
@@ -97,12 +97,27 @@ public class Say : MonoBehaviour
                     text.transform.GetChild(1).GetChild(0).GetComponent<TMP_Text>().text = dialog.personage.name;
                     playerImage.sprite = dialog.personage.image != null ? dialog.personage.image : null;
                     break;
+                case AuthorText authorText:
+                    author.transform.GetChild(0).GetComponent<TMP_Text>().text = authorText.text;
+                    break;
             }
             if (_dialog.GetOutputPort("output").Connection != null)
             {
                 _dialog = _dialog.GetOutputPort("output").Connection.node;
             }
         }
+    }
+    
+    IEnumerator UpdateHeight(TMP_Text tx, RectTransform txRect, RectTransform authorRect)
+    {
+        // Ждем следующего кадра
+        yield return new WaitForEndOfFrame();
+
+        // Теперь можно обновить preferredHeight
+        tx.text = tx.text + "\n s";
+        txRect.sizeDelta = new Vector2(875, tx.preferredHeight);
+        authorRect.sizeDelta = new Vector2(900, tx.preferredHeight + 100);
+        tx.text = "";
     }
     
     private void ShowText(string message, GameObject obj)
@@ -227,14 +242,11 @@ public class Say : MonoBehaviour
     
     private void ShowDialog(Dialog dialog)
     {
-        TMP_Text tx = text.transform.GetChild(0).GetComponent<TMP_Text>();
-        tx.text = dialog.Text + "\n s";
-        tx.GetComponent<RectTransform>().sizeDelta = new Vector2(875, tx.preferredHeight);
-        text.GetComponent<RectTransform>().sizeDelta = new Vector2(900, tx.preferredHeight + 100);
-        tx.text = "";
-        RectTransform txRect = tx.GetComponent<RectTransform>();
+        RectTransform txRect = text.transform.GetChild(0).GetComponent<RectTransform>();
         RectTransform childRect = text.transform.GetChild(1).GetComponent<RectTransform>();
         text.transform.GetChild(1).GetChild(0).GetComponent<TMP_Text>().text = dialog.personage.name;
+        playerImage.color= new Color(playerImage.color.r, playerImage.color.g, playerImage.color.b, 1f);
+        playerImage.gameObject.SetActive(true);
         playerImage.sprite = dialog.personage.image != null ? dialog.personage.image : null;
         txRect.anchorMin = new Vector2(0.5f, 0);
         txRect.anchorMax = new Vector2(0.5f, 0);
@@ -244,6 +256,11 @@ public class Say : MonoBehaviour
         childRect.anchorMax = new Vector2(0, 1);
         childRect.pivot = new Vector2(0, 1);
         childRect.anchoredPosition = new Vector2(50, -30);
+        TMP_Text tx = text.transform.GetChild(0).GetComponent<TMP_Text>();
+        tx.text = dialog.Text + "\n s";
+        tx.GetComponent<RectTransform>().sizeDelta = new Vector2(875, tx.preferredHeight);
+        text.GetComponent<RectTransform>().sizeDelta = new Vector2(900, tx.preferredHeight + 100);
+        tx.text = "";
         ShowText(dialog.Text, text);
     }
     
@@ -254,11 +271,6 @@ public class Say : MonoBehaviour
 
     private void author_Text(AuthorText aT)
     {
-        TMP_Text tx = author.transform.GetChild(0).GetComponent<TMP_Text>();
-        tx.text = aT.text + "\n s";
-        tx.GetComponent<RectTransform>().sizeDelta = new Vector2(900, tx.preferredHeight);
-        author.GetComponent<RectTransform>().sizeDelta = new Vector2(900, tx.preferredHeight + 100);
-        tx.text = "";
         RectTransform authorRect = author.GetComponent<RectTransform>();
         authorRect.anchorMin = new Vector2(0.5f, 0.5f);
         authorRect.anchorMax = new Vector2(0.5f, 0.5f);
@@ -267,6 +279,11 @@ public class Say : MonoBehaviour
         author.SetActive(true);
         text.SetActive(false);
         playerImage.color= new Color(playerImage.color.r, playerImage.color.g, playerImage.color.b, 0f);
+        TMP_Text tx = author.transform.GetChild(0).GetComponent<TMP_Text>();
+        tx.text = aT.text + "\n s";
+        tx.GetComponent<RectTransform>().sizeDelta = new Vector2(875, tx.preferredHeight);
+        author.GetComponent<RectTransform>().sizeDelta = new Vector2(900, tx.preferredHeight + 30);
+        tx.text = "";
         ShowText(aT.text, author);
     }
     
