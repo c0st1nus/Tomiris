ccd0a6b650147bb5b3a6ddf6bfa4b87c3e6d86f9
y1202m <139690404+y1202m@users.noreply.github.com>
2024-03-05 00:59:12 +0600

    wowow

--- Assets/Scripts/Node/StoryGraph.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using XNode;
-
-[CreateAssetMenu(fileName = "New Scene", menuName = "Game/New Scene")]
-public class StoryGraph : NodeGraph {
-	public Node current;
-}
